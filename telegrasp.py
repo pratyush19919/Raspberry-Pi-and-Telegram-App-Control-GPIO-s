94bb23bd10ba392c6c6742496c6b4081b532d7bf
pratyush19919 <52246459+pratyush19919@users.noreply.github.com>
2020-08-22 02:36:42 +0530

    Update telegrasp.py

--- telegrasp.py
+++ telegrasp.py
@@ -1,8 +1,10 @@
-import telepot
+# import packages
+
+import telepot   #To communicate with Telegram API we are going to use python library called telepot.
 import time,datetime
 import RPi.GPIO as GPIO
 from telepot.loop import MessageLoop
-
+#set up pins
 GPIO.setmode(GPIO.BOARD)
 Relay1=16
 motor=10
@@ -19,18 +21,20 @@ GPIO.output(Relay1,0)
 GPIO.setup(Relay2,GPIO.OUT)
 GPIO.output(Relay2,0)
 
-
 def action(msg):
+    #function for decoding what is there in the message we typed in raspberry pi
+
     chat_id=msg["chat"]["id"]
     command=msg["text"]
     print('Received: %s' % command)
     print(msg["chat"]["id"])
+    # if date is typed
     if "date" in command:
         message="The Date is "+str(datetime.datetime.now().strftime("%d/%m/%Y"))
         telegram_bot.sendMessage(chat_id,message)
         
 
-
+#for on commands
     if "on" in command:
         message="Turned On"
         if "motor" in command:
@@ -43,9 +47,9 @@ def action(msg):
             GPIO.output(Relay2,1)
             message=message + "Relay2"
 
-        telegram_bot.sendMessage(chat_id, message)
+        telegram_bot.sendMessage(chat_id, message) #replying to the message
 
-
+#for on commands
     if "off" in command:
         message="Turned Off"
         if "motor" in command:
@@ -60,10 +64,10 @@ def action(msg):
 
         telegram_bot.sendMessage(chat_id, message)
 
-telegram_bot=telepot.Bot("1247076443:AAEjJ8YC0rz4ASy3d_fPmQAJF7q3RPniO4Y")
+telegram_bot=telepot.Bot("*********************************************") #THIS IS WHERE YOU PASTE YOUR API TOKEN INSIDE QUOTES
 print(telegram_bot.getMe())
 
-MessageLoop(telegram_bot,action).run_as_thread()
+MessageLoop(telegram_bot,action).run_as_thread()  #Looping the function 'action' 
 print("started and running")
 
 
