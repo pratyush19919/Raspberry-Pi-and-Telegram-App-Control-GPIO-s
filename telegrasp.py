bf73a0919e831e2a2ae8dcd7c9a3e29f121cfeaf
pratyush19919 <52246459+pratyush19919@users.noreply.github.com>
2020-09-11 00:33:06 +0530

    Update telegrasp.py
    With the help of a web-scraping technique, by asking your telegram chatbot for news, it replies you with the top news headlines of the day, making the chatbot much more useful and efficient.

--- telegrasp.py
+++ telegrasp.py
@@ -1,19 +1,22 @@
-# import packages
-
-import telepot   #To communicate with Telegram API we are going to use python library called telepot.
+#import required libraries & packages
+import telepot  #for telegram
 import time,datetime
 import RPi.GPIO as GPIO
+import requests #for web-scraping
+import random
+from bs4 import BeautifulSoup # for parsing
 from telepot.loop import MessageLoop
-#set up pins
+
 GPIO.setmode(GPIO.BOARD)
+#setting & initializing pins
 Relay1=16
-motor=10
+led =10
 Relay2=12
 
 GPIO.setwarnings(False)
 
-GPIO.setup(motor,GPIO.OUT)
-GPIO.output(motor,0)
+GPIO.setup(led,GPIO.OUT)
+GPIO.output(led,0)
 
 GPIO.setup(Relay1,GPIO.OUT)
 GPIO.output(Relay1,0)
@@ -21,53 +24,77 @@ GPIO.output(Relay1,0)
 GPIO.setup(Relay2,GPIO.OUT)
 GPIO.output(Relay2,0)
 
-def action(msg):
-    #function for decoding what is there in the message we typed in raspberry pi
 
-    chat_id=msg["chat"]["id"]
-    command=msg["text"]
-    print('Received: %s' % command)
+def action(msg):
+    chat_id=msg["chat"]["id"] #chat_id contains the header information for message
+    command=msg["text"]  #command contains the text that we write in chat
+    print('Received: %s' %command)
     print(msg["chat"]["id"])
-    # if date is typed
-    if "date" in command:
+    if "date" in command:    # to get date
         message="The Date is "+str(datetime.datetime.now().strftime("%d/%m/%Y"))
         telegram_bot.sendMessage(chat_id,message)
+    
+    if "time" in command:   # to get time
+        message="The Time is "+str(datetime.datetime.now().strftime("%H:%M:%S"))
+        telegram_bot.sendMessage(chat_id,message)
+    # to get top 10 headlines of news     
+    if "news" in command:  # if your message containes "news" , it replies you top headlines of the day
+        message="<==:::: Today's headlines are ::::==> \n"
+        for i, x in enumerate(scrape()):
+            message += "----------------------------\n"
+            message += str(i+1) + " " + x + "\n"
+        telegram_bot.sendMessage(chat_id,message)
         
+# to control raspberry pi gpio's 
 
-#for on commands
     if "on" in command:
         message="Turned On"
-        if "motor" in command:
-            GPIO.output(motor,1)
-            message=message + "motor"
-        if "Relay1" in command:
-            GPIO.output(Relay1,1)
-            message=message + "Relay1"
-        if "Relay2" in command:
-            GPIO.output(Relay2,1)
-            message=message + "Relay2"
-
-        telegram_bot.sendMessage(chat_id, message) #replying to the message
-
-#for on commands
-    if "off" in command:
-        message="Turned Off"
-        if "motor" in command:
-            GPIO.output(motor,0)
-            message=message + "motor"
+        if "Led" in command:
+            GPIO.output(led,1)
+            message=message + " Led"
         if "Relay1" in command:
             GPIO.output(Relay1,0)
-            message=message + "Relay1"
+            message=message + " Relay1"
         if "Relay2" in command:
             GPIO.output(Relay2,0)
-            message=message + "Relay2"
+            message=message + " Relay2"
+
 
         telegram_bot.sendMessage(chat_id, message)
 
-telegram_bot=telepot.Bot("*********************************************") #THIS IS WHERE YOU PASTE YOUR API TOKEN INSIDE QUOTES
+
+    if "off" in command:
+        message="Turned Off"
+        if "Led" in command:
+            GPIO.output(led,0)
+            message=message + " Led"
+        if "Relay1" in command:
+            GPIO.output(Relay1,1)
+            message=message + " Relay1"
+        if "Relay2" in command:
+            GPIO.output(Relay2,1)
+            message=message + " Relay2"
+        #print(chat_id)
+        telegram_bot.sendMessage(chat_id, message)
+        
+        
+def scrape():# Function for scraping the news website for getting the headlines
+    news=[]
+    url = "https://www.indiatoday.in/news.html"  # url of website that we want to scrape
+    res=requests.get(url)
+    code=BeautifulSoup(res.text,"lxml")
+    head=code.find_all("p",class_="story")
+    for i in range(0,10):
+        news.append(str(head[random.randint(0,len(head))-1].text))
+    return news #returns list of headlines in the website
+
+
+telegram_bot=telepot.Bot("******************************************")#API Key you get from the bot-father in telegram app
+
 print(telegram_bot.getMe())
 
-MessageLoop(telegram_bot,action).run_as_thread()  #Looping the function 'action' 
+
+MessageLoop(telegram_bot,action).run_as_thread()
 print("started and running")
 
 
